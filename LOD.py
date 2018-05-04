f096296e619afdad92756675c00a096a83cdf4be
Xiaodi Pan <xiaodip@andrew.cmu.edu>
2018-05-03 20:03:47 -0400

    new

--- LOD.py
+++ LOD.py
@@ -8,6 +8,7 @@ def parse_arguments():
     parser.add_argument('--RNASeq', dest='RNASeq', type=str, required=True)
     parser.add_argument('--RiboSeq', dest='RiboSeq', type=str, required=True)
     parser.add_argument('--f', dest='f', type=int, default=0.9)
+    parser.add_argument('--n', dest='n', type=int, default=25)
 
     return parser.parse_args()
     
@@ -15,42 +16,52 @@ def parse_arguments():
 def exe_commands(args):
 
     # Create directory to store intermediate files
-
+    
     os.popen("mkdir intermediate").read()
-    # STAR
+    # HISAT2
     print("Running HISAT2 ...")
 
-    os.popen("STAR --runThreadN 7 --runMode genomeGenerate --genomeDir ./outputs/reference_genome/ --genomeFastaFiles "+args.refFa+"--sjdbGTFfile "+args.refGTF+"--sjdb 29").read()
-    os.popen("STAR --runThreadN 7 --genomeDir ./outputs/reference_genome/ --readFilesIn "+args.RNASeq+"--outFileNamePrefix RNAseq").read()
+    os.popen("hisat2_extract_splice_sites.py " + args.refGTF + "> ./intermediate/reference.ss").read()
+    os.popen("hisat2_extract_exons.py " + rargs.refGTF + "> ./intermediate/reference.exon").read()
+    os.popen("Hisat2-build -p " + args.n + "--ss ./intermediate/reference.ss --exon ./intermediate/reference.exon " + args.refFa + "./intermediate/reference_ht2_index").read()
+    os.popen("Hisat2 -p 15 -x ./intermediate/reference_ht2_index -U " + args.RNAseq + "-S ./intermediate/RNAseq.alignedto.reference.sam").read()
     print("Finished!")
+
 
     # Samtools
     print("Running Samtools ...")
-    os.popen("Samtools view -bS RNAseq.Aligned.out.sam | samtools sort > ./outputs/RNAseq.sorted.bam").read()
+    os.popen("Samtools view -bS ./intermediate/RNAseq.alignedto.reference.sam | samtools sort > ./intermediate/RNAseq.sorted.bam").read()
     print("Finished!")
 
     # StringTie
     print("Running StringTie ...")
-    os.popen("stringtie ./outputs/RNAseq.sorted.bam -f "+args.f+"-p 16 -G "+args.refGTF+"-o ./outputs/RNAseq.transcriptome.gtf").read()
+    os.popen("stringtie ./intermediate/RNAseq.sorted.bam -f " + args.f + "-p 16 -G " + args.refGTF +"-o ./intermediate/RNAseq.transcriptome.gtf").read()
     print("Finished!")
 
+    # preprocess
+    print("Preprocessing ...")
+    os.popen("python preprocessGTF.py ./intermediate/RNAseq.transcriptome.gtf "+ args.refGTF +"./intermediate/RNAseq.processed.transcriptome.gtf").read()
+    os.popen("Python ChangeName.py ./intermediate/RNAseq.processed.transcriptome.gtf ./intermediate/RNAseq.newnames.transcriptome.gtf").read()
+    print("Finished!")
+    
     # STAR
     print("Running STAR ...")
-    os.popen("STAR --runThreadN 7 --genomeDir ./outputs/experimental_genome/ --genomeFastaFiles "+args.refFa+"--sjdbGTFfile ./outputs/RNAseq.transcriptome.gtf").read()
-    os.popen("STAR --outFilterType BySJout --runThreadN 7 --outFilterMismatchNmax 2 --genomeDir ./outputs/experimental_genome/ --readFilesIn "+args.RiboSeq+"--outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM GeneCounts --outFilterMultmapNmax 1 --outFilterMatchNmin 16 --alignEndsType EndToEnd").read()
-    os.popen("python preprocessGTF.py ./outputs/RNAseq.transcriptome.gtf "+args.refGTF+"RNAseq.processed.transcriptome.gtf").read()
+    os.popen("STAR --runThreadN " + args.n + "--genomeDir ./intermediate/experimental_genome/ --genomeFastaFiles " + args.refFa + "--sjdbGTFfile ./intermediate/RNAseq.newnames.transcriptome.gtf").read()
+    os.popen("STAR --outFilterType BySJout --runThreadN " + args.n + "--outFilterMismatchNmax 2 --genomeDir ./intermediate/experimental_genome/ --readFilesIn "+ args.RiboSeq +"--outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM GeneCounts --outFilterMultmapNmax 1 --outFilterMatchNmin 16 --alignEndsType EndToEnd --outFileNamePrefix ./intermediate/Riboseq").read()
+    
     print("Finished!")
-
+    
     # RiboCode
     print("Running RiboCode ...")
-    os.popen("prepare_transcripts -g RNAseq.processed.transcriptome.gtf -o ./outputs/RiboCode_annot -f "+args.RiboSeq).read()
-    os.popen("metaplots -a ./outputs/RiboCode_annot -r Riboseq.AlignedTo.Transcriptome.Bam").read()
-    os.popen("RiboCode -a ./outputs/RiboCode_annot -c config.txt -l no -g -o RiboCode_ORFs_result").read()
+    os.popen("GTFupdate ./intermediate/RNAseq.newnames.transcriptome.gtf > ./intermediate/RNAseq.processed.updated.gtf").read()
+    os.popen("prepare_transcripts -g ./intermediate/RNAseq.processed.updated.gtf -o ./intermediate/RiboCode_annot -f " + args.refFa).read()
+    os.popen("metaplots -a ./intermediate/RiboCode_annot -r ./intermediate/Riboseq.AlignedTo.Transcriptome.Bam").read()
+    os.popen("RiboCode -a ./intermediate/RiboCode_annot -c metaplots_pre_config.txt -l no -g -o RiboCode_ORFs_result").read()
     print("Finished!")
-
+    
 def main(args):
 
-    #args = parse_arguments()
+    args = parse_arguments()
     exe_commands(args)
     
 if __name__ == '__main__':
