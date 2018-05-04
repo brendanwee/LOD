52557f935fa4376d17c525592b2f2531532ae062
Bweestyle <bwee@andrew.cmu.edu>
2018-05-03 22:39:05 -0400

    allow index

--- LOD.py
+++ LOD.py
@@ -41,16 +41,7 @@ def exe_commands(args):
 
     os.popen("mkdir intermediate").read()
     # HISAT2
-    print("Running HISAT2 ...")
-    print "extracting splice sites"
-    os.popen(bin + "hisat2_extract_splice_sites.py " + args.refGTF + " > ./intermediate/reference.ss").read()
-    print "extracting exons"
-    os.popen(bin + "hisat2_extract_exons.py " + args.refGTF + " > ./intermediate/reference.exon").read()
-    print "building index"
-    os.popen(bin + "hisat2-build -p " + str(args.n) + " --ss ./intermediate/reference.ss --exon ./intermediate/reference.exon " + args.refFa + " ./intermediate/reference_ht2_index").read()
-    print "aligning reads"
-    os.popen(bin + "hisat2 -p 15 -x ./intermediate/reference_ht2_index -U " + args.RNASeq + " -S ./intermediate/RNAseq.alignedto.reference.sam").read()
-    print("Finished!")
+
 
     if(args.i==1):
         # HISAT2
@@ -63,6 +54,11 @@ def exe_commands(args):
         os.popen(bin + "hisat2-build -p " + str(args.n) + " --ss ./intermediate/reference.ss --exon ./intermediate/reference.exon " + args.refFa + " ./intermediate/reference_ht2_index").read()
         print "aligning reads"
         os.popen(bin + "hisat2 -p 15 -x ./intermediate/reference_ht2_index -U " + args.RNASeq + "-S ./intermediate/RNAseq.alignedto.reference.sam").read()
+        print("Finished!")
+    else:
+        print("Index Provided: Running HISAT2 ...")
+        os.popen(
+            bin + "hisat2 -p 15 -x "+ args.i + " -U " + args.RNASeq + " -S ./intermediate/RNAseq.alignedto.reference.sam").read()
         print("Finished!")
 
 
